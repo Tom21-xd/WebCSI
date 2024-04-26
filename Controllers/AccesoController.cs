0818f7be3c4e14499054cbf1891de4f1b4236837
Johan Steven Ramirez Murcia <johans.ramirez@udla.edu.co>
2024-04-26 14:53:40 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCSI.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        public IActionResult Recuperar()
+        {
+            return View();
+        }
+    }
+}
